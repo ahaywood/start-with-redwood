ba6d6ebc1f2d0eb21d57cba85fb9f2cca370b0af
Amy Haywood Dutton <me@amyhaywood.com>
2024-07-27 08:40:19 -0500

    🚧 WIP: Stubbed out the basic Group Page

--- web/src/pages/GroupsPage/GroupsPage.tsx
+++ web/src/pages/GroupsPage/GroupsPage.tsx
@@ -1,19 +1,32 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
+import PageHeader from 'src/components/admin/PageHeader/PageHeader'
+
 const GroupsPage = () => {
   return (
     <>
       <Metadata title="Groups" description="Groups page" />
 
-      <h1>GroupsPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/GroupsPage/GroupsPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>groups</code>, link to me with `
-        <Link to={routes.groups()}>Groups</Link>`
-      </p>
+      <PageHeader
+        title="Groups"
+        backLink={routes.users()}
+        breadcrumbs={[
+          { name: 'Users', href: routes.users() },
+          { name: 'Groups', href: routes.groups() },
+        ]}
+      />
+
+      <div className="main-padding">
+        <h1>GroupsPage</h1>
+        <p>
+          Find me in <code>./web/src/pages/GroupsPage/GroupsPage.tsx</code>
+        </p>
+        <p>
+          My default route is named <code>groups</code>, link to me with `
+          <Link to={routes.groups()}>Groups</Link>`
+        </p>
+      </div>
     </>
   )
 }
