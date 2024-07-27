4b87f2a1a417d0d32c3cfd5b4a3f826428e7df01
Amy Haywood Dutton <me@amyhaywood.com>
2024-07-27 08:40:50 -0500

    🚧 WIP: Started styling the Users Page

--- web/src/pages/UsersPage/UsersPage.tsx
+++ web/src/pages/UsersPage/UsersPage.tsx
@@ -1,19 +1,47 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Form } from '@redwoodjs/forms'
+import { routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+import ActionBar from 'src/components/admin/ActionBar/ActionBar'
+import Icon from 'src/components/admin/Icon/Icon'
+import PageHeader from 'src/components/admin/PageHeader/PageHeader'
+import SearchBar from 'src/components/admin/SearchBar/SearchBar'
 
 const UsersPage = () => {
   return (
     <>
       <Metadata title="Users" description="Users page" />
 
-      <h1>UsersPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/UsersPage/UsersPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>users</code>, link to me with `
-        <Link to={routes.users()}>Users</Link>`
-      </p>
+      <PageHeader
+        title="Manage Users"
+        backLink={routes.dashboard()}
+        breadcrumbs={[{ name: 'Users', href: routes.users() }]}
+      />
+
+      <div className="main-padding">
+        <div className="mb-8 flex items-center justify-between">
+          {/* search bar */}
+          <Form className="flex-1">
+            <SearchBar placeholder="Search for a Specific User" />
+          </Form>
+
+          {/* add a new user button */}
+          <button className="button solid primary shorter">+ User</button>
+        </div>
+
+        {/* users table */}
+      </div>
+
+      {/* action buttons */}
+      <ActionBar>
+        <>
+          <button className="button text no-padding">Cancel</button>
+          <button className="button solid destructive short">
+            <Icon id="trash" />
+            Delete
+          </button>
+        </>
+      </ActionBar>
     </>
   )
 }
