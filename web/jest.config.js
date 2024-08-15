c365f9ab785332500bb85055944167a1607c79c2
Amy Haywood Dutton <me@amyhaywood.com>
2024-08-15 15:41:53 -0500

    ✨ Set up a path alias
    - Needed to ad to Jest specifically so it would not break tests

--- web/jest.config.js
+++ web/jest.config.js
@@ -3,6 +3,9 @@
 const config = {
   rootDir: '../',
   preset: '@redwoodjs/testing/config/jest/web',
+  moduleNameMapper: {
+    '^@startRW/(.*)$': '<rootDir>/web/src/components/admin/$1',
+  },
 }
 
 module.exports = config
